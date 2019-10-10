f9e501e7e33ee8b3ca038e1a5dd0c6a86d0cc116
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 16:30:21 +0900

    Smaller than or equal to comparison

---
+++ HomOper.c/HomCompSE.c
@@ -0,0 +1,13 @@
+void HomCompSE(LweSample* res, const LweSample* a, const LweSample* b, const int length, const TFheGateBootstrappingCloudKeySet* bk) {
+
+	LweSample* temp = new_gate_bootstrapping_ciphertext_array(2, bk->params);
+	
+	bootsCONSTANT(&temp[0], 0, bk);
+	
+	for(int i = 0; i < length; i++){
+		bootsXNOR(&temp[1], &a[i], &b[i], bk);
+		bootsMUX(&temp[0], &temp[1], &temp[0], &a[i], bk);}
+
+	bootsNOT(&res[0], &temp[0], bk);
+	delete_gate_bootstrapping_ciphertext_array(2, temp);
+}

4a1fc2d493d319c8ea479b6e26d0a2b90d695d6b
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 16:34:44 +0900

    Create HomMax.c

---
+++ HomOper.c/HomMax.c
@@ -0,0 +1,13 @@
+// if a > b then res = a
+
+void HomMax(LweSample* res, const LweSample* a, const LweSample* b, const int length, const TFheGateBootstrappingCloudKeySet* bk) {
+
+	LweSample* temp = new_gate_bootstrapping_ciphertext_array(1, bk->params);
+	
+	HomCompB(&temp[0], a, b, length, bk);
+
+	for(int i = 0; i < length; i++){
+		bootsMUX(&res[i], &temp[0], &a[i], &b[i], bk);}
+
+	delete_gate_bootstrapping_ciphertext_array(1, temp);
+}

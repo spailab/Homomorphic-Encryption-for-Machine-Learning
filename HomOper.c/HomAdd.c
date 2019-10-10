4b1a17917b5f9ef3c263c8b8448e420170f4a31c
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 16:08:28 +0900

    Addition

---
+++ HomOper.c/HomAdd.c
@@ -0,0 +1,23 @@
+//res = a + b
+
+void HomAdd(LweSample* res, const LweSample* a, const LweSample* b, const int length, const TFheGateBootstrappingCloudKeySet* bk) {
+
+	LweSample* c = new_gate_bootstrapping_ciphertext_array(length, bk->params);	    
+	LweSample* temp = new_gate_bootstrapping_ciphertext_array(2, bk->params);	    
+
+	bootsCONSTANT(&c[0], 0, bk);
+    
+	for(int i = 0; i < length -1; i++){
+		bootsXOR(&temp[0], &a[i], &b[i], bk);
+		bootsAND(&temp[1], &a[i], &b[i], bk);
+		bootsXOR(&res[i], &temp[0], &c[i], bk);
+		bootsAND(&temp[0], &temp[0], &c[i], bk);
+		bootsOR(&c[i+1], &temp[0], &temp[1], bk);
+	}
+
+	bootsXOR(&temp[0], &a[length-1], &b[length-1], bk);
+	bootsXOR(&res[length-1], &temp[0], &c[length-1], bk);
+
+	delete_gate_bootstrapping_ciphertext_array(length, c);    
+	delete_gate_bootstrapping_ciphertext_array(2, temp);    
+}

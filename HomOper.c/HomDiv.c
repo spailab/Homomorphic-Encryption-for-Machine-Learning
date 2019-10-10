800b7c491c98581dfca939a283b28b776f975b8f
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 17:13:20 +0900

    Division

---
+++ HomOper.c/HomDiv.c
@@ -0,0 +1,96 @@
+// a / (-b)  =>  (-a) / b
+// (-a) / (-b)  =>  a / b
+// length * length = length
+// res1 = Quotient
+// res2 = Reminder
+
+void HomDiv(LweSample* res1, LweSample* res2, const LweSample* a, const LweSample* b, const int length, const TFheGateBootstrappingCloudKeySet* bk) {
+	
+	LweSample* temp = new_gate_bootstrapping_ciphertext_array(3, bk->params);
+	LweSample* A = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* B = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* QR = new_gate_bootstrapping_ciphertext_array(2*length, bk->params);
+	LweSample* D = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* C = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* Q = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* DD = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* R = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* E0 = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	
+	HomAbs(A, a, length, bk);
+	HomAbs(B, b, length, bk);
+	//printf("absolute value clear\n");
+
+	for(int i = 0; i < length; i++){
+		bootsCOPY(&QR[i], &A[i], bk);
+		bootsCOPY(&D[i], &B[i], bk);
+		bootsCONSTANT(&QR[length + i], 0, bk);}
+	
+
+	HomRShift(QR, QR, 2*length, 1, bk);
+	//printf("stting clear\n");
+	for(int s = 1; s < length; s++){
+		HomRShift(QR, QR, 2*length, 1, bk);
+
+		for(int i = 0; i < length; i++){
+			bootsCOPY(&R[i], &QR[length+i], bk);}
+
+		HomCompS(&temp[0], R, D, length, bk);
+		bootsNOT(&temp[1], &temp[0], bk);
+		bootsCOPY(&QR[0], &temp[1], bk);
+
+		for(int i = 0; i < length; i++){
+			bootsAND(&DD[i], &D[i], &temp[1], bk);}
+
+		HomSubt(R, R, DD, length, bk);
+
+		for(int i = 0; i < length; i++){
+			bootsCOPY(&QR[length+i], &R[i], bk);}
+		
+	//	printf("%d loop clear\n",s);
+	}
+		
+
+
+	for(int i = 0; i < length; i++){
+		bootsCOPY(&Q[i], &QR[i], bk);
+		bootsCOPY(&res2[i], &QR[length+i], bk);}
+
+
+
+	HomTwosCompliment(C, Q, length, bk);
+	
+	bootsXOR(&temp[0], &a[length-1], &b[length-1], bk);
+	bootsNOT(&temp[1], &temp[0], bk);
+
+	for(int i = 0; i < length; i++){
+		bootsAND(&Q[i], &Q[i], &temp[1], bk);
+		bootsAND(&C[i], &C[i], &temp[0], bk);}
+
+	HomAdd(Q, Q, C, length, bk);
+
+	for(int i = 0; i < length; i++){
+		bootsCONSTANT(&E0[i], 0, bk);}
+
+	//printf("666666666\n");
+	HomEqui(&temp[0], res2, E0, length, bk);
+
+	//printf("777777777\n");
+	bootsNOT(&E0[0], &temp[0], bk);
+	//printf("8\n");
+
+	HomSubt(res1, Q, E0, length, bk);
+	//printf("0\n");
+
+
+	delete_gate_bootstrapping_ciphertext_array(3, temp);
+	delete_gate_bootstrapping_ciphertext_array(length, A);
+	delete_gate_bootstrapping_ciphertext_array(length, B);
+	delete_gate_bootstrapping_ciphertext_array(2*length, QR);
+	delete_gate_bootstrapping_ciphertext_array(length, D);
+	delete_gate_bootstrapping_ciphertext_array(length, DD);
+	delete_gate_bootstrapping_ciphertext_array(length, Q);
+	delete_gate_bootstrapping_ciphertext_array(length, R);
+	delete_gate_bootstrapping_ciphertext_array(length, C);
+	delete_gate_bootstrapping_ciphertext_array(length, E0);
+}

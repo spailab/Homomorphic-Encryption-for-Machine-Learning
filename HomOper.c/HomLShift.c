437367abea0b46ea850f7f1eff992dc543a4a671
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 16:18:16 +0900

    Left Shift

---
+++ HomOper.c/HomLShift.c
@@ -0,0 +1,9 @@
+void HomLShift(LweSample* res, const LweSample* a, const int length, const int k, const TFheGateBootstrappingCloudKeySet* bk) {
+
+	for(int i = 0; i < length - k; i++){
+		bootsCOPY(&res[i], &a[i+k], bk);}
+	for(int i = length-k; i < length; i++){
+		bootsCOPY(&res[i], &a[length-1], bk);}
+}
+
+

391f56c9eccb3f7e367fe4337f6279d0e8b19794
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-10 16:14:00 +0900

    Two's Compliment

---
+++ HomOper.c/HomTwosCom.c
@@ -0,0 +1,23 @@
+//res = -a
+
+void HomTwosCompliment(LweSample* res, const LweSample* a, const int length, const TFheGateBootstrappingCloudKeySet* bk) {
+	LweSample* temp = new_gate_bootstrapping_ciphertext_array(1, bk->params);	    
+	LweSample* b = new_gate_bootstrapping_ciphertext_array(length, bk->params);	    
+
+	bootsCONSTANT(&b[0], 1, bk);
+    
+
+	for(int i = 0; i < length - 2; i++){
+		bootsNOT(&temp[0], &a[i], bk);
+		bootsXOR(&res[i], &temp[0], &b[i], bk);
+		bootsAND(&b[i+1], &temp[0], &b[i], bk);}
+
+	bootsNOT(&temp[0], &a[length-2], bk);
+	bootsXOR(&res[length-2], &temp[0], &b[length-2], bk);
+
+	bootsNOT(&res[length-1], &a[length-1], bk);
+	
+
+	delete_gate_bootstrapping_ciphertext_array(length, b);    
+	delete_gate_bootstrapping_ciphertext_array(1, temp);    
+}

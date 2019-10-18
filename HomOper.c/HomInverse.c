84cd4b366c4e49b765a57c23b0455816934f25b4
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2019-10-18 15:40:07 +0900

    Inverse matrix - Gauss jordan elimination method

--- HomOper.c/HomInverse.c
+++ HomOper.c/HomInverse.c
@@ -1,64 +1,70 @@
-void HomInverse(LweSample* res[10][10], LweSample* x[10][10], const int N, const int length, const TFheGateBootstrappingCloudKeySet* bk){
-	LweSample* ratio = new_gate_bootstrapping_ciphertext_array(length, bk->params);
-	LweSample* ratio2 = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* a = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* A = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	
- 	printf("00");
- 	for (int i = 0; i < N; i++){
- 		for (int j = N; j <2*N;j++){
- 			if (i==(j-N))
- 				for(int s = 0; s < length; s++){
- 					for(int t = 0; t < length; t++){
-		 				bootsCONSTANT(&x[s][t], 0, bk);
-		 			}
- 						bootsCONSTANT(&x[(length/2)-1][(length/2)-1], 1, bk);
- 				}		
- 				//x[i][j] = 1;
- 			else
- 				for(int s = 0; s < length; s++){
- 					for(int t = 0; t < length; t++){
-		 				bootsCONSTANT(&x[s][t], 0, bk);
-		 			}
- 				}
- 				//x[i][j] = 0;
- 		}
- 	}
+void HomInv(LweSample* res, const LweSample ** a, const int N, const int length, const TFheGateBootstrappingCloudKeySet* bk){
+	LweSample* c = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+	LweSample* d = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+    LweSample* L = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+    LweSample* K = new_gate_bootstrapping_ciphertext_array(length, bk->params);              
+	LweSample* f = new_gate_bootstrapping_ciphertext_array(2, bk->params);	    
+    LweSample* A = new_gate_bootstrapping_ciphertext_array(length, bk->params);
 
- 	printf("1");
+    //find dimensions of input matrix
+    int C = N; int r = N;
 
- 	for (int i=0; i<N; i++){
- 		for(int j=0;j<N;j++){
- 			if(i!=j){
- 			HomDiv(ratio, A, x[j][i], x[i][i],length, bk);
- 			printf("11");
- 			for (int k=0;k<2*N;k++){
- 				HomMultiReal(ratio2, ratio, x[i][k], length, bk);
- 				HomSubt(x[j][k], x[j][k], ratio2, length, bk);
- 				printf("22");
- 				}
- 			}
- 		}
-	}
-	printf("2");
-	
-	for(int i=0;i<N;i++){
-		bootsCOPY(&a, &x[i][i], bk);
-		for (int j=0;j<2*N;j++){
-		HomDiv(x[i][j], A, x[i][j], a, length, bk);
-		}
-	}	
+    HomEye(f[M][M],a[N][N],N,length, bk);
+     /// Let's start inverse
+    for (int  j=0; j<r; i++)
+    {
+    		for (int i=0; i<r;i++)
+    		{
+            	bootsCONSTANT(&s,0,bk);
+              {
+             	    for (int k = 0; k < r; k++)
+             		{
+             			bootsMUX(&s, &a[i][j], &a[j][k], &s, bk);
+             			bootsMUX(&a[j][k], &a[i][j], &a[i][k], &a[j][k],bk);
+             			bootsMUX(&a[i][k], &a[i][j], &s, &a[i][k], bk);
+             			
+                        bootsMUX(&s, &a[i][j], &f[j][k], &s, bk);
+             			bootsMUX(&f[j][k], &a[i][j], &f[i][k], &f[j][k], bk);
+             			bootsMUX(&f[i][k], &a[i][j], &s, &f[i][k], bk);
+             		}
 
-	for(int i=0;i<length;i++)
-	{
-		for(int j=N;j<2*length;j++)
-		{
-		bootsCOPY(&res[i][j], &x[i][j], bk);	
-		}
-	}
+                    
+                    HomDiv(t, A, 1, a[j,j], length,bk);
 
-	delete_gate_bootstrapping_ciphertext_array(length, ratio);
- 	delete_gate_bootstrapping_ciphertext_array(length, ratio2);
- 	delete_gate_bootstrapping_ciphertext_array(length, a);
- 	delete_gate_bootstrapping_ciphertext_array(length, A);
- }	
+             		for (int k=0;k <N; k++)
+             		{
+             			HomMulti(a[j][k], t, a[j][k], length,bk);
+             			HomMulti(f[j][k], t, f[j][k], length,bk);
+             		}
+             		for (int L=0;L<N;L++)
+             		{
+             			//bootsCONSTANT(&sel, L ~= j, bk);
+             			bootsCONSTANT(&t,0,bk);
+             			HomTwosCompilment(t,a[L][j],length,bk);
+                   
+             			for (int k=0;k<N;k++)
+             			{
+             				HomMulti(c,t,a[j][k],length,bk);
+                            HomMulti(d,t,f[j][k],length,bk);
+             				HomAdd(a[L][k],a[L][k],c,length,bk);
+          				  	HomAdd(f[L][k],f[L][k],d,length,bk);
+             			}	
+             		}	
+
+              } 		
+    		}
+     }
+   
+    for(int i = 0; i < length; i++){
+        for(int j = 0; j< length; j++){
+        bootsCOPY(&res[i][j], &f[L][k], bk);
+        }
+    }    
+//delete bootstrapping ciphertext arrays
+	delete_gate_bootstrapping_ciphertext_array(length, c);
+	delete_gate_bootstrapping_ciphertext_array(length, d);
+	delete_gate_bootstrapping_ciphertext_array(length, L);
+	delete_gate_bootstrapping_ciphertext_array(length, K);
+	delete_gate_bootstrapping_ciphertext_array(length, f);
+	delete_gate_bootstrapping_ciphertext_array(length, A);
+}    

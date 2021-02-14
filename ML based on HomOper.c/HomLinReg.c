47458b87eb9212c41d9cd4b5927b0a9266805252
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2021-02-14 23:54:31 +0900

    Update HomLinReg.c

--- ML based on HomOper.c/HomLinReg.c
+++ ML based on HomOper.c/HomLinReg.c
@@ -1,3 +1,4 @@
+# Linear Regression
 void HomLinReg(LweSample* res[2], LweSample **x, LweSample **y, const int N, const int length, const TFheGateBootstrappingCloudKeySet* bk){
 
 	LweSample* beta0 = new_gate_bootstrapping_ciphertext_array(length, bk->params);

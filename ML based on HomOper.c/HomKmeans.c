fa6aa77db12c10cf4048d67086ad02002a208df8
Signal Processing and Advanced Intelligence Lab <55823661+spailab@users.noreply.github.com>
2021-06-21 15:21:56 +0900

    Update HomKmeans.c

--- ML based on HomOper.c/HomKmeans.c
+++ ML based on HomOper.c/HomKmeans.c
@@ -1,19 +1,35 @@
 # k-means 
-void HomKmeans(LweSample* res[10], const LweSample* a[100], const LweSample* u[10], const int length, const int N, const int k, const TFheGateBootstrappingCloudKeySet* bk){
+void HomKmeans(LweSample* res[100], LweSample* m[100], LweSample **a, LweSample **u, const int length, const int N, const int k, const TFheGateBootstrappingCloudKeySet* bk){
 
 
- 	LweSample *L[N], *M[N], *U[N], *w[k-1], *ww[k-1], *v[k-1];
+	int length2 = log2(N)+1;
 
-
+ 	LweSample *L[N], *E[N], *U[k], *G[N], *F[k], *H[k], *w[k-1], *ww[k-1], *v[k-1];
+	LweSample *M[N];
+	// L = data copy // 
+	// M = label // 
+	// E = label copy for add // 
+	// G = L and M // 
  	for(int i = 0; i < N; i++)
  	{
  		L[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
  		M[i] = new_gate_bootstrapping_ciphertext_array(k, bk->params);
- 		U[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+ 		E[i] = new_gate_bootstrapping_ciphertext_array(length2, bk->params);
+ 		G[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
  	}
 
+	// F = # of data in each cluster //
+	// H = sum of data in esch cluster //
+	// U = H / F //
+ 	for(int i = 0; i < k; i++)
+ 	{
+ 		F[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+ 		H[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
+ 		U[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
 
+	}
 
+	// variable for labelling //
  	for(int i = 0; i < k-1; i++)
  	{
  		w[i] = new_gate_bootstrapping_ciphertext_array(length, bk->params);
@@ -21,17 +37,10 @@ void HomKmeans(LweSample* res[10], const LweSample* a[100], const LweSample* u[1
  		v[i] = new_gate_bootstrapping_ciphertext_array(2, bk->params);
 	}
 
+	LweSample* t = new_gate_bootstrapping_ciphertext_array(k, bk->params);
 
-
- 	LweSample* A = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* B = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* C = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* D = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* UU = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* t = new_gate_bootstrapping_ciphertext_array(k, bk->params);
- 	LweSample* E = new_gate_bootstrapping_ciphertext_array(length, bk->params);
- 	LweSample* temp = new_gate_bootstrapping_ciphertext_array(3, bk->params);
-
+	// data copy //
+	// a[N][length] to L[N][length] //
  	for(int i = 0; i < N; i++)
 	{
  		for(int j = 0; j < length; j++)
@@ -40,116 +49,168 @@ void HomKmeans(LweSample* res[10], const LweSample* a[100], const LweSample* u[1
  		}
  	}
 
- 	printf("start algorithm!\n");
+ 	// initial value setting for counting and sum of each cluster's data //
+ 	// E[N][length2] = 0 //
+ 	// F[k][length] = {0, 0, ..., N} (# of data) //
+ 	// H[k][length] = 0 //
+	for(int i = 0; i < N; i++)
+	{
+		for(int j = 0; j < length2; j++)
+			bootsCONSTANT(&E[i][j], 0, bk);
+	}
+
+	for(int i = 0; i < k; i++)
+	{
+		for(int j = 0; j < length; j++)
+		{
+			bootsCONSTANT(&F[i][j], 0, bk);
+			bootsCONSTANT(&H[i][j], 0, bk);
+		}
+	}
+
+	bootsCONSTANT(&F[k-1][length2-1], 1, bk);
+
+
+
+
  	int s = 0;
  	while(s < 1){
- 		printf("means\n");
- 		for(int i = 0; i < k-1; i++)
+ 		// calculate median of near cluster's means //
+ 		// w[i] = median(mean) of u[i] and u[i+1] //
+		for(int i = 0; i < k-1; i++)
  		{
- 			printf("%d-th means\n", i+1);
  			HomAdd(ww[i], u[i], u[i+1], length, bk);
- 			printf("%d-th means\n", i+1);
- 			HomRShift(w[i], ww[i], length, 1, bk);
- 			printf("%d-th means\n", i+1);
+ 			HomLShift(w[i], ww[i], length, 1, bk);
  		}
- 		printf("%d - calculating means of near cluster clear!\n", s+1);
 
+ 		printf("%d - calculating median of cluster's means clear!\n", s+1);
+
+ 		// label setting process //
  		for(int i = 0; i < N; i++)
  		{
+ 			// comparison with data and median //
+ 			// if L[i] > w[i], then v[j][1] = 1 //
  			for(int j = 0; j < k-1; j++)
  			{
  				HomCompB(&v[j][1], L[i], w[j], length, bk);
  				bootsNOT(&v[j][0], &v[j][1], bk);
  			}
 
- 			bootsCONSTANT(&t[0], 1, bk);
+ 			// label setting //
+ 			// ex) M[i][j] = {100, 010, 001} //
  			for(int j = 0; j < k; j++)
  			{
- 				for(int m = 0; m < k-1; m++)
+ 				bootsCONSTANT(&t[0], 1, bk);
+
+ 				for(int m = 1; m < k; m++)
  				{
  					int e;
- 					if(m < j)
+ 					if(m <= j)
  						e = 1;
  					else
  						e = 0;
 
- 					bootsAND(&t[m+1], &v[m][e], &t[m], bk);
+ 					bootsAND(&t[m], &v[m-1][e], &t[m-1], bk);
  				}
 
- 				M[i][j] = t[k-1];
+ 				bootsCOPY(&M[i][j], &t[k-1], bk);
  			}
-
+ 			printf("%d - %d - label setting process clear!\n", s+1, i+1);
  		}
-
- 		printf("%d - label setting clear\n", s+1);
-
-
-
- 		bootsCONSTANT(&E[0], 0, bk);
- 		for(int i = 0; i < length-1; i++)
- 			bootsAND(&E[i+1], &E[i], &E[0], bk);
-
- 		for(int r = 0; r < k; r++)
- 		{
- 			for(int i = 0; i < N; i++)
- 			{
- 				for(int j = 0; j < length; j++)
- 				{
- 					bootsAND(&U[i][j], &L[i][j], &M[i][r], bk);
- 				}
+ 		
+ 		for(int i = 0; i < N; i++){
+ 			for(int j = 0; j < k; j++){
+ 				bootsCOPY(&m[i][j], &M[i][j], bk);
  			}
+ 		}
+/*
+ 	for(int i = 0; i < N; i++)
+ 	{
+ 		for(int j = 0; j < k; j++)
+			bootsCOPY(&res[i][j], &M[i][j], bk);
+ 	}
+*/
 
 
- 			int m = 0; 
- 			int NN = N;
- 			while(m <  log2(N))
+ 		// counting # of each cluster's data : F //
+ 		// sum of each cluster's data : H //
+ 		// new_cluster mean u[i] = H[i]/F[i] //
+ 		// i = 0 ... k-2 //
+		for(int i = 0; i < k-1; i++)
+		{
+			bootsCOPY(&F[i][0], &M[0][i], bk);
+			for(int j = 1; j < N; j++)
 			{
- 				for(int i = 0; i < NN/2; i++)
- 				{
- 					int ii = 2*i;
- 					HomEqui(&temp[0], U[ii], E, length, bk);
- 					HomEqui(&temp[1], U[ii+1], E, length, bk);
- 					for(int j = 0; j < length; j++)
- 					{
-						bootsAND(&A[j], &U[ii][j], &temp[1], bk);
- 						bootsAND(&B[j], &U[ii+1][j], &temp[0], bk);
- 					}
+				bootsCOPY(&E[j][0], &M[j][i], bk);
+				HomAdd(F[i], F[i], E[j], length2, bk);
+			}
 
- 					HomAdd(C, U[ii], A, length, bk);
- 					HomAdd(D, C, U[ii+1], length, bk);
- 					HomAdd(UU, D, B, length, bk);
- 					HomRShift(U[i], UU, length, 1, bk);
- 				}
- 				NN = NN/2;
- 				m++;
- 			}
+			for(int j = 0; j < N; j++)
+			{
+				for(int p = 0; p < length; p++)
+				{
+					bootsAND(&G[j][p], &L[j][p], &M[j][i], bk);
+				}
+ 				
+				HomAdd(H[i], H[i], G[j], length, bk);
+					
+			}
 
- 			bootsCONSTANT(&temp[2], 1, bk);
 
- 			for(int i = 0; i < length; i++)
- 			{ 
- 				bootsAND(&u[r][i], &U[0][i], &temp[2], bk);
- 			}
+			HomDiv(u[i], U[i], H[i], F[i], length, bk);
+		
+ 			printf("%d - %d cluster mean calculate complete\n", s+1,i+1);
+			
+			HomSubt(F[k-1], F[k-1], F[i], length2, bk);
 
- 			printf("%d - %d - cluster fixed\n", s+1, r+1);
+		}
+		// i = k-1 //
+		for(int j = 0; j < N; j++)
+		{
+			for(int p = 0; p < length; p++)
+			{
+				bootsAND(&G[j][p], &L[j][p], &M[j][k-1], bk);
+			}
+			
+			HomAdd(H[k-1], H[k-1], G[j], length, bk);
+				
+		}
+ 		
+		
+		HomDiv(u[k-1], U[k-1], H[k-1], F[k-1], length, bk);	
 
- 		}
- 		s++;
+		printf("%d - %d cluster mean calculate complete\n", s+1,k);
+		
+
+		s++;
 
  		printf("%d - loop clear\n", s);
+
+
  	}
 
-
+ 	// result //
+ 	// res[i] = u[i] //
  	for(int i = 0; i < k; i++)
  	{
  		for(int j = 0; j < length; j++)
- 			res[i][j] = u[i][j];
+			bootsCOPY(&res[i][j], &u[i][j], bk);
  	}
 
+
+ 	//clean up all pointers //
  	for(int i = 0; i < N; i++)
  	{
  		delete_gate_bootstrapping_ciphertext_array(length, L[i]);
+ 		delete_gate_bootstrapping_ciphertext_array(length2, E[i]);
  		delete_gate_bootstrapping_ciphertext_array(k, M[i]);
+ 		delete_gate_bootstrapping_ciphertext_array(length, G[i]);
+ 	}
+
+ 	for(int i = 0; i < k; i++)
+ 	{
+ 		delete_gate_bootstrapping_ciphertext_array(length, F[i]);
+ 		delete_gate_bootstrapping_ciphertext_array(length, H[i]);
  		delete_gate_bootstrapping_ciphertext_array(length, U[i]);
  	}
 
@@ -160,13 +221,7 @@ void HomKmeans(LweSample* res[10], const LweSample* a[100], const LweSample* u[1
  		delete_gate_bootstrapping_ciphertext_array(2, v[i]);
  	}
 
- 	delete_gate_bootstrapping_ciphertext_array(length, A);
- 	delete_gate_bootstrapping_ciphertext_array(length, B);
- 	delete_gate_bootstrapping_ciphertext_array(length, C);
- 	delete_gate_bootstrapping_ciphertext_array(length, D);
- 	delete_gate_bootstrapping_ciphertext_array(length, UU);
  	delete_gate_bootstrapping_ciphertext_array(k, t);
- 	delete_gate_bootstrapping_ciphertext_array(length, E);
- 	delete_gate_bootstrapping_ciphertext_array(3, temp);
 
  }
+
